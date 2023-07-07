d69bffa9969596216ba4febb6f4af4b7c970e5fb
YONGWOOK CHOI <60510921+CYY1007@users.noreply.github.com>
2023-07-08 00:58:01 +0900

    Issue/1 : health check를 위한 API 추가 (#6)
    * CI : deploy.yml 수정
    
    * CI : Procfile 추가
    
    * CI : makeFiles 추가
    
    * CI : GET /health 추가, health check를 위함

---
+++ src/main/java/zipdabang/server/web/controller/RootController.java
@@ -0,0 +1,13 @@
+package zipdabang.server.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String healthAPi(){
+        return "i'm healthy";
+    }
+}
