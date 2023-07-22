2a4f566200d62a70346a1f6bc0df8ab8b7ab6d16
YONGWOOK CHOI <60510921+CYY1007@users.noreply.github.com>
2023-07-22 23:41:32 +0900

    refactor, bug : domain계층 버그 수정 (#21)

--- src/main/java/zipdabang/server/domain/InfoAgree.java
+++ src/main/java/zipdabang/server/domain/InfoAgree.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DynamicInsert
 @DynamicUpdate
+@Entity
 public class InfoAgree extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
