c4a2791803e9f9f16ff7e18f410e0cff908b5249
CYY1007 <younguk86@gmail.com>
2023-08-17 20:05:15 +0900

    :hammer: Feat : 내가본 물품 Dto 작성

---
+++ src/main/java/zipdabang/server/web/dto/responseDto/MarketResponseDto.java
@@ -0,0 +1,53 @@
+package zipdabang.server.web.dto.responseDto;
+
+import lombok.*;
+
+import java.util.List;
+
+public class MarketResponseDto {
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    private static class ProductCategoryDto{
+        private Long categoryId;
+        private String name;
+        private String categoryImageUrl;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    private static class ProductDto{
+        private Long productId;
+        private Long productCategoryId;
+        private String productImageUrl;
+        private String productName;
+        private String price;
+        private Float productScore;
+        private Boolean isInBasket;
+        private Boolean isLiked;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class WatchedProductPreviewDto{
+        private List<ProductCategoryDto> productCategoryList;
+        private List<ProductDto> productList;
+        private Integer categoryListSize;
+        private Integer productListSize;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class WatchedProductDto{
+        private List<ProductDto> productList;
+        private Integer productListSize;
+    }
+}
