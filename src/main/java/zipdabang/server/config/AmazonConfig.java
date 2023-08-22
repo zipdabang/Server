4eaddbbf762b6bef2d4f80011be20a08e8a70834
YONGWOOK CHOI <60510921+CYY1007@users.noreply.github.com>
2023-08-22 18:58:07 +0900

    :sparkles: Feat : S3 Config (#68)

---
+++ src/main/java/zipdabang/server/config/AmazonConfig.java
@@ -0,0 +1,61 @@
+package zipdabang.server.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+@Getter
+public class AmazonConfig {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.s3.folder.zipdabang-recipe-thumbNail}")
+    private String recipeThumbnail;
+
+    @Value("${cloud.aws.s3.folder.zipdabang-recipe-steps}")
+    private String recipeStep;
+
+    @Value("${cloud.aws.s3.folder.zipdabang-proifile}")
+    private String userProfile;
+
+    private AWSCredentials awsCredentials;
+
+    @PostConstruct
+    public void init() {
+        this.awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    }
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+
+    @Bean
+    public AWSCredentialsProvider awsCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(awsCredentials);
+    }
+}
