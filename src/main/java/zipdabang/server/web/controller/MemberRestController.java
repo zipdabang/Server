e54c6fa82e3361bdfbd59a27d188d23daeb56dcc
CYY1007 <younguk86@gmail.com>
2023-09-04 20:13:44 +0900

    :memo: Docs : 나중에 로그인하기 API swagger 수정

--- src/main/java/zipdabang/server/web/controller/MemberRestController.java
+++ src/main/java/zipdabang/server/web/controller/MemberRestController.java
@@ -226,7 +226,7 @@ public class MemberRestController {
         return ResponseDto.of(MemberConverter.toTermsDto(memberService.getAllTerms()));
     }
 
-    @Operation(summary = "🎪figma[온보딩1] 나중에 로그인하기 API ", description = "나중에 로그인하기 API 입니다.")
+    @Operation(summary = "🎪figma[온보딩1] 나중에 로그인하기 API ✔️", description = "나중에 로그인하기 API 입니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "2000",description = "OK 성공, access Token 하나만 반환함"),
             @ApiResponse(responseCode = "5000",description = "SERVER ERROR, 백앤드 개발자에게 알려주세요",content = @Content(schema = @Schema(implementation = ResponseDto.class))),
