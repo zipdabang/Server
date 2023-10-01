ea3161f4a5f8e69faef7c387cced20500a65b25a
HyoBin Yang <50162252+HyoBN@users.noreply.github.com>
2023-10-01 16:19:28 +0900

    Feature/140 Conflict Resolve (#151)
    * :sparkles: 유저 차단하기 API
    
    * :art: 사용하지 않는 API 삭제
    
    * :sparkles: 유저 차단 해지, 차단 목록 조회 API
    
    * Conflict Resolve
    
    * Conflict Resolve

--- src/main/java/zipdabang/server/web/controller/MemberRestController.java
+++ src/main/java/zipdabang/server/web/controller/MemberRestController.java
@@ -24,6 +24,10 @@ import zipdabang.server.auth.handler.annotation.AuthMember;
 import zipdabang.server.base.Code;
 import zipdabang.server.base.ResponseDto;
 import zipdabang.server.base.exception.handler.MemberException;
+<<<<<<< HEAD
+import zipdabang.server.base.exception.handler.RecipeException;
+=======
+>>>>>>> a5dc21e0132e8f3e84329ab4d71729bbff08fef8
 import zipdabang.server.converter.MemberConverter;
 import zipdabang.server.domain.Category;
 import zipdabang.server.domain.member.Follow;
@@ -315,8 +319,8 @@ public class MemberRestController {
     @Parameters({
             @Parameter(name = "member", hidden = true),
     })
-    @PostMapping(value = "/members/inquiries",consumes ={ MediaType.MULTIPART_FORM_DATA_VALUE } )
-    public ResponseDto<MemberResponseDto.MemberInqueryResultDto> createInquery(@CheckTempMember @AuthMember Member member, @ModelAttribute @Valid MemberRequestDto.InqueryDto request){
+    @PostMapping(value = "/members/inquiries", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseDto<MemberResponseDto.MemberInqueryResultDto> createInquery(@CheckTempMember @AuthMember Member member, @ModelAttribute @Valid MemberRequestDto.InqueryDto request) {
         Inquery inquery = memberService.createInquery(member, request);
         return ResponseDto.of(MemberConverter.toMemberInqueryResultDto(inquery));
     }
@@ -333,11 +337,12 @@ public class MemberRestController {
             @ApiResponse(responseCode = "4055", description = "BAD_REQEUST , 페이지 번호가 초과함", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
 
     })
-    public ResponseDto<MemberResponseDto.InqueryListDto> showInquery(@CheckTempMember @AuthMember Member member, @RequestParam(name = "page",required = true) @CheckPage Integer page){
+    public ResponseDto<MemberResponseDto.InqueryListDto> showInquery(@CheckTempMember @AuthMember Member member, @RequestParam(name = "page", required = true) @CheckPage Integer page) {
         Page<Inquery> inqueryPage = memberService.findInquery(member, page);
         return ResponseDto.of(MemberConverter.toInqueryListDto(inqueryPage));
     }
-    @Operation(summary = "[figma 더보기 - 회원 탈퇴] 회원 탈퇴 API ✔️🔑", description = "회원 탈퇴 API입니다.<br> 테스트를 위해 임시로 해당 유저의 상세주소를 \"TEST\" 로 설정하면(상세정보 수정 API - zipCode) 탈퇴 불가능한 경우로 처리되도록 해놨습니다.<br> deregisterTypes 종류 <br>"+
+
+    @Operation(summary = "[figma 더보기 - 회원 탈퇴] 회원 탈퇴 API ✔️🔑", description = "회원 탈퇴 API입니다.<br> 테스트를 위해 임시로 해당 유저의 상세주소를 \"TEST\" 로 설정하면(상세정보 수정 API - zipCode) 탈퇴 불가능한 경우로 처리되도록 해놨습니다.<br> deregisterTypes 종류 <br>" +
             "- NOTHING_TO_BUY(\"사고싶은 물건이 없어요.\"),<br>" +
             "- DISINTERESTED(\"앱을 이용하지 않아요.\"),<br>" +
             "- UNCOMFORTABLE(\"앱 이용이 불편해요.\"),<br>" +
@@ -411,6 +416,7 @@ public class MemberRestController {
         Page<Member> blockedMembers = memberService.findBlockedMember(page, member);
         return ResponseDto.of(MemberConverter.toPagingMemberListDto(blockedMembers));
     }
+}
 
 
     @Operation(summary = "🎪팔로우하기 API", description = "팔로우하기 API 입니다.")
