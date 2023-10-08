ade1e9b4a7d2a6f419d2a24e231186d831b35626
HyoBN <qls0786@naver.com>
2023-10-08 19:10:32 +0900

    :bug: 내가 차단한 유저 조회 쿼리문 수정

--- src/main/java/zipdabang/server/repository/memberRepositories/BlockedMemberRepository.java
+++ src/main/java/zipdabang/server/repository/memberRepositories/BlockedMemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import zipdabang.server.domain.member.BlockedMember;
 import zipdabang.server.domain.member.Member;
 
@@ -13,8 +14,10 @@ import java.util.Optional;
 public interface BlockedMemberRepository extends JpaRepository<BlockedMember, Long> {
     List<BlockedMember> findByOwner(Member owner);
 
-    @Query("select b.blocked from BlockedMember b")
-    Page<Member> findBlockedByOwner(Member owner, PageRequest pageRequest);
+    @Query("select b.blocked from BlockedMember b where b.owner = :owner ")
+    List<Member> findBlockedByOwner(Member owner);
+    @Query("select b.blocked from BlockedMember b where b.owner = :owner ")
+    Page<Member> findBlockedByOwner(@Param("owner") Member owner, PageRequest pageRequest);
     Optional<BlockedMember> findByOwnerAndBlocked(Member owner, Member blocked);
 
     boolean existsByOwnerAndBlocked(Member owner, Member blocked);
