bc927993d5dd733157479a8fc03423980b3fadfb
Hanvp <nwactris@naver.com>
2023-10-27 13:56:33 +0900

    :recycle: Feature/238 베스트 레시피 시간 재셋팅, 삭제 허용

--- src/main/java/zipdabang/server/repository/recipeRepositories/WeeklyBestRecipeRepository.java
+++ src/main/java/zipdabang/server/repository/recipeRepositories/WeeklyBestRecipeRepository.java
@@ -1,7 +1,12 @@
 package zipdabang.server.repository.recipeRepositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import zipdabang.server.domain.recipe.Recipe;
 import zipdabang.server.domain.recipe.WeeklyBestRecipe;
 
 public interface WeeklyBestRecipeRepository extends JpaRepository<WeeklyBestRecipe, Long> {
+    Boolean existsByRecipe(Recipe findRecipe);
+
+    void deleteByRecipe(Recipe findRecipe);
+
 }
